a77a4601ecf3cb7590a774f37f562fcb228b8b1c
supertypo <suprtypo@pm.me>
2025-10-21 21:01:59 +0200

    Increased expire time to 7 days

--- manager.go
+++ manager.go
@@ -47,11 +47,11 @@ const (
 
 	// defaultStaleGoodTimeout is the time in which a previously reachable
 	// node is considered stale.
-	defaultStaleGoodTimeout = time.Minute * 30
+	defaultStaleGoodTimeout = time.Minute * 15
 
 	// defaultStaleBadTimeout is the time in which a previously unreachable
 	// node is considered stale.
-	defaultStaleBadTimeout = time.Hour
+	defaultStaleBadTimeout = time.Hour * 2
 
 	// dumpAddressInterval is the interval used to dump the address
 	// cache to disk for future use.
@@ -66,7 +66,7 @@ const (
 
 	// pruneExpireTimeout is the expire time in which a node is
 	// considered dead.
-	pruneExpireTimeout = time.Hour * 24
+	pruneExpireTimeout = time.Hour * 24 * 7
 )
 
 // NewManager constructs and returns a new dnsseeder manager, with the provided dataDir
